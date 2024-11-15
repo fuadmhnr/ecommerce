9391aa94c7799b1457453457d8417920ec3904ff
fuadmhnr <fuadmhnr@gmail.com>
2024-11-16 06:32:06 +0700

    fix: overlapping image and navbar on product detail

--- resources/views/livewire/product-detail-page.blade.php
+++ resources/views/livewire/product-detail-page.blade.php
@@ -3,28 +3,37 @@
         <div class="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
             <div class="flex flex-wrap -mx-4">
                 <div class="w-full mb-8 md:w-1/2 md:mb-0" x-data="{ mainImage: '{{ url('storage', $product->images[0]) }}' }">
-                    <div class="sticky top-0 z-50 overflow-hidden ">
-                        <div class="relative mb-6 lg:mb-10 lg:h-2/4 ">
-                            <img x-bind:src="mainImage" alt="" class="object-cover w-full lg:h-full ">
+                    <!-- Changed z-index and added proper spacing -->
+                    <div class="sticky top-20 z-30 overflow-hidden bg-white dark:bg-gray-800">
+                        <!-- Added proper aspect ratio container -->
+                        <div class="relative mb-6 lg:mb-10">
+                            <div class="aspect-w-16 aspect-h-12">
+                                <img x-bind:src="mainImage" alt="{{ $product->name }}" class="object-cover w-full">
+                            </div>
                         </div>
-                        <div class="flex-wrap hidden md:flex ">
+                        
+                        <!-- Thumbnail gallery -->
+                        <div class="flex-wrap hidden md:flex">
                             @foreach ($product->images as $image)
-                                <div class="w-1/2 p-2 sm:w-1/4"
+                                <div class="w-1/2 p-2 sm:w-1/4" 
                                     x-on:click="mainImage='{{ url('storage', $image) }}'">
-                                    <img src="{{ url('storage', $image) }}" alt="{{ $product->name }}"
-                                        class="object-cover w-full lg:h-20 cursor-pointer hover:border hover:border-blue-500">
+                                    <div class="aspect-w-1 aspect-h-1">
+                                        <img src="{{ url('storage', $image) }}" 
+                                            alt="{{ $product->name }}"
+                                            class="object-cover w-full cursor-pointer transition-all duration-300 hover:border-2 hover:border-blue-500">
+                                    </div>
                                 </div>
                             @endforeach
                         </div>
-                        <div class="px-6 pb-6 mt-6 border-t border-gray-300 dark:border-gray-400 ">
+                
+                        <!-- Shipping info section -->
+                        <div class="px-6 pb-6 mt-6 border-t border-gray-300 dark:border-gray-400">
                             <div class="flex flex-wrap items-center mt-6">
                                 <span class="mr-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                         fill="currentColor" class="w-4 h-4 text-gray-700 dark:text-gray-400 bi bi-truck"
                                         viewBox="0 0 16 16">
-                                        <path
-                                            d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z">
-                                        </path>
+                                        <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path>
                                     </svg>
                                 </span>
                                 <h2 class="text-lg font-bold text-gray-700 dark:text-gray-400">Free Shipping</h2>
